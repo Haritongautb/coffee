92be91849938bf2518ec8692be040df2ecf15bb4
Hariton <khariton.aaa.97@gmail.com>
2023-10-13 18:29:46 +0200

    Update App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const coffeeData = useHttp();
   const bestCoffee = useHttp();
 
-  console.log(bestCoffee);
+  console.log("update");
   React.useEffect(() => {
     (async () => {
       await coffeeData.request("https://6501d205736d26322f5c5699.mockapi.io/coffeeData");
