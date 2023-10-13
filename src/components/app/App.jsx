f21c46ff15cf1ff761b1cf8fb056a338425c3d00
Hariton <khariton.aaa.97@gmail.com>
2023-10-13 18:22:45 +0200

    Update App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -24,8 +24,8 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/coffee" element={<MainLayout />}>
-        <Route index element={<Home status={bestCoffee.statusLoading} data={bestCoffee.data} />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<Home status={bestCoffee.statusLoading} data={bestCoffee.data} />} />
         <Route path="/ourCoffee" element={<Coffee Component={OurCoffee} status={coffeeData.statusLoading} data={coffeeData.data} />} />
         <Route path="/ourCoffee/:idCoffee" element={<Coffee Component={SingleCoffeePage} title="The Coffee AROMISTICO" />} />
         <Route path="/forYourPleasure" element={<ForYourPleasure status={coffeeData.statusLoading} data={coffeeData.data} />} />
